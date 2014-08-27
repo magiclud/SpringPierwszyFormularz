04c4d3c9544fa86507a1fc50665487698ebcd99f
magiclud <agnieszkalud@op.pl>
2014-08-27 20:15:16 +0200

    still work, I added dependecies - validation

--- src/main/java/net/codejava/spring/model/User.java
+++ src/main/java/net/codejava/spring/model/User.java
@@ -2,9 +2,20 @@ package net.codejava.spring.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+ 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class User {
     private String username;
     private String password;
+    @NotEmpty @Email
     private String email;
     private Date birthDate;
     private String profession;
